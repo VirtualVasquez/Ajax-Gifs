36655c82601e902bd7552796fc5f183ea7cad06c
VirtualVasquez <mxv647@med.miami.edu>
2018-11-28 15:47:24 -0500

    updating css/html, WIP: bring buttons to the side

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,8 +1,7 @@
 //VARIABLES AND FUNCTIONS
 //===================================================================================
-var topics = ['My Hero Academia', 'DragonBall', 'Full Metal Alchemist',
-'Naruto', 'Attack on Titan', 'One Punch Man', 'Yu-Gi-Oh', 'Digimon', 'Pokemon',
-'Bleach']
+var topics = ['Attack on Titan', 'Bleach', 'Digimon', 'DragonBall', 'Full Metal Alchemist', 'My Hero Academia', 'Naruto', 'One Punch Man', 'Pokemon', 'Yu-Gi-Oh' ]
+var newTopics = [];
 
 //MAKE A LOOP THAT APPENDS TOPICS ARRAY TO #BUTTONS
 function renderButtons(){
@@ -22,14 +21,31 @@ function renderButtons(){
   }
 };
 
+function renderNewButtons(){
+  $('#newButtons').empty();
+  //looping through the array of topics
+  for (var i = 0; i < newTopics.length; i++){
+    //dynamically generates buttons for each topic in the array
+    var b = $('<button>');
+    //adding a class of topic-btn to our button
+    b.addClass('topic');
+    //adding a data-attribute
+    b.attr('data-name', newTopics[i]);
+    //providing the initial button text
+    b.text(newTopics[i]);
+    //adding the button to the buttons-view div
+    $('#newButtons').append(b);
+  }  
+}
+
 //MAIN PROCESSES
 //===================================================================================
 
 $(document).ready(function(){
+  renderButtons();
 
   $('#gifHolder').delegate('img.gif', 'click', function(){
     var state = $(this).data('state');
-    console.log(state);
     //if gif is paused, click will animate
     if (state == 'still'){
       state = 'animate';
@@ -48,23 +64,17 @@ $(document).ready(function(){
     event.preventDefault();
     //take the string from "#animeInput" and add it to the array
     var newButton = $("#animeInput").val().trim();
-      topics.push(newButton);
+      newTopics.push(newButton);
       $('#animeInput').val('');
-      renderButtons();
+      renderNewButtons();
   }); 
 
-  renderButtons();
   //WHEN #buttons CLICKED...
   //THEN GENERATE 10 PAUSED GIFS WITH RATING
   $("#buttons").delegate("button.topic", "click", function(){
-
-    var show = $(this).attr('data-name');
-    
-    console.log(show);
-    
+    var show = $(this).attr('data-name');    
     //clear #gifHolder
     $('#gifHolder').empty();
-    
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
     show + "&api_key=aMuZaMUV4XlGlcpfWZ91SRE076EX4Av3";
     $.ajax({
@@ -75,10 +85,40 @@ $(document).ready(function(){
         var results = response.data;
         //generates 10 GIFs, hence i < 10 
         for (var i = 0; i < 10; i++){
-          var gifDiv = $("<div class ='item'>"),
+          var gifDiv = $("<div class='item col-xs-12'>"),
+              rating = results[i].rating,
+              p = $("<p>").addClass('col-xs-12').text("Rating: " + rating);
+          var showImage = $("<img>");
+          showImage.attr("src", results[i].images.fixed_height_still.url)
+                   .data('still', results[i].images.fixed_height_still.url)
+                   .data('animate', results[i].images.fixed_height.url)
+                   .data('state', 'still')
+                   .addClass('gif col-xs-12');
+          //***append the gifs to #gifHolder paused
+          gifDiv.prepend(p).prepend(showImage);
+          //***below every gif, display its rating
+          $("#gifHolder").prepend(gifDiv);
+        }
+    });
+  });
+
+  $("#newButtons").delegate("button.topic", "click", function(){
+    var show = $(this).attr('data-name');    
+    //clear #gifHolder
+    $('#gifHolder').empty();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+    show + "&api_key=aMuZaMUV4XlGlcpfWZ91SRE076EX4Av3";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then(function(response){
+        var results = response.data;
+        //generates 10 GIFs, hence i < 10 
+        for (var i = 0; i < 10; i++){
+          var gifDiv = $("<div class ='item col-xs-6'>"),
               rating = results[i].rating,
               p = $("<p>").text("Rating: " + rating);
-          
           var showImage = $("<img>");
           showImage.attr("src", results[i].images.fixed_height_still.url)
                    .data('still', results[i].images.fixed_height_still.url)
